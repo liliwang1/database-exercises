9585b055da3415765927743d8db06887ea09e065
liliwang <monsi0510@hotmail.com>
2020-10-26 16:08:07 -0500

    feat: update albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    release_date SMALLINT,
-    sales DOUBLE,
+    name VARCHAR(50) NOT NULL DEFAULT 'Untitled',
+    release_date SMALLINT UNSIGNED,
+    sales DOUBLE UNSIGNED,
     genre VARCHAR(100),
     PRIMARY KEY (id)
 );
