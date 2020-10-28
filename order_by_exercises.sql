9bac6df2a05da7b1921319c0a93fd131f643fd67
liliwang <monsi0510@hotmail.com>
2020-10-28 11:09:16 -0500

    feat: Add LIMIT exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -25,4 +25,4 @@ SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
   AND birth_date LIKE '%-12-25'
-ORDER BY hire_date DESC, birth_date DESC;
+ORDER BY hire_date DESC, birth_date;
