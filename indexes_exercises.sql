84da8257669b52ad91d61c939db8e04f0806514a
liliwang <monsi0510@hotmail.com>
2020-10-29 14:44:41 -0500

    feat: finish index exercises

---
+++ indexes_exercises.sql
@@ -0,0 +1,19 @@
+USE employees;
+
+DESCRIBE departments;
+
+DESCRIBE dept_emp;
+
+DESCRIBE dept_manager;
+
+DESCRIBE employees;
+
+DESCRIBE salaries;
+
+DESCRIBE titles;
+
+
+USE codeup_test_db;
+
+ALTER TABLE albums
+    ADD UNIQUE (artist, name);
