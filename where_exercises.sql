fdc6300693856c072aebd044bc6f06158c9a0682
liliwang <monsi0510@hotmail.com>
2020-10-29 14:43:49 -0500

    refactor: update the exercises after instructor walkthrough

--- where_exercises.sql
+++ where_exercises.sql
@@ -11,6 +11,7 @@ WHERE last_name LIKE 'e%';
 SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+-- WHERE hire_date LIKE '199%';
 
 SELECT *
 FROM employees
@@ -22,32 +23,36 @@ WHERE last_name LIKE '%q%';
 
 SELECT *
 FROM employees
-WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+WHERE first_name = 'Irena'
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya';
 
 SELECT *
 FROM employees
 WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
-AND gender = 'M';
+  AND gender = 'M';
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'e%'
-OR last_name LIKE '%e';
+   OR last_name LIKE '%e';
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'e%'
-   AND last_name LIKE '%e';
+  AND last_name LIKE '%e';
 
 SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-AND birth_date LIKE '%-12-25';
+  AND birth_date LIKE '%-12-25';
 
 SELECT *
 FROM employees
-WHERE last_name REGEXP 'q' AND last_name NOT REGEXP 'qu';
+WHERE last_name REGEXP 'q'
+  AND last_name NOT REGEXP 'qu';
 
 SELECT *
 FROM employees
-WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
