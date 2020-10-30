25e8fe81dabb2ea4224c7ccfa3948b57831dd64b
liliwang <monsi0510@hotmail.com>
2020-10-30 11:46:35 -0500

    feat: update sub-query exercise

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -3,21 +3,22 @@ USE employees;
 -- Find all the employees with the same hire date as employee 101010
 SELECT *
 FROM employees
-WHERE hire_date IN (
+WHERE hire_date = (
     SELECT hire_date
     FROM employees
     WHERE emp_no = 101010
     );
 
 -- Find all the titles held by all employees with the first name Aamod
--- SELECT title
-SELECT DISTINCT title
+SELECT title, COUNT(title)
+-- SELECT DISTINCT title
 FROM titles
 WHERE emp_no IN (
     SELECT emp_no
     FROM employees
     WHERE first_name = 'Aamod'
-);
+)
+GROUP BY title;
 
 -- Find all the current department managers that are female
 SELECT first_name, last_name
