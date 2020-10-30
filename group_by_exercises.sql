64c7f917c40bcbbd6fe2b5cef2d0bc50e0b9a9a1
liliwang <monsi0510@hotmail.com>
2020-10-30 10:17:16 -0500

    feat: add finished GROUP BY exercises

---
+++ group_by_exercises.sql
@@ -0,0 +1,29 @@
+USE employees;
+
+SELECT DISTINCT title
+FROM titles;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY last_name;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY last_name, first_name;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT first_name, last_name, COUNT(*) AS count
+FROM employees
+GROUP BY last_name, first_name HAVING COUNT(*) > 1
+ORDER BY COUNT(*) DESC;
+
+SELECT COUNT(*) AS count, gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
